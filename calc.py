a5573fab1ee14c4e1f87fd9977e510d6cc83d240
jay-fibi <jay.gohil@fibilabs.tech>
2024-02-09 20:01:20 +0530

    updated message to be more clear.

--- calc.py
+++ calc.py
@@ -30,7 +30,7 @@ while True:
             num1 = float(input("Enter first number: "))
             num2 = float(input("Enter second number: "))
         except ValueError:
-            print("Invalid input. Please enter a number.")
+            print("Invalid input. You simply cannot enter anything else than number...")
             continue
 
         if choice == '1':
