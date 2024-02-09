4f857058f7eb6abf6026c3b1cda541a9dcaaf978
jay-fibi <jay.gohil@fibilabs.tech>
2024-02-09 21:14:58 +0530

    multi-file commit1

--- fibo.py
+++ fibo.py
@@ -1,11 +1,24 @@
-def fibonacci(n):
-  if n <= 1:
-    return n
-  else:
-    return fibonacci(n-1) + fibonacci(n-2)
+from typing import Dict
 
-n = int(input("How many terms? "))
+fib_cache: Dict[int, int] = {0: 0, 1: 1} 
 
-print("Fibonacci sequence:")
-for i in range(n):
-  print(fibonacci(i))
+def fibonacci(n: int) -> int:
+    if n in fib_cache:
+        return fib_cache[n]
+
+    nth_fib = fibonacci(n-1) + fibonacci(n-2)
+    fib_cache[n] = nth_fib
+    return nth_fib
+
+num_terms = 0
+while num_terms <= 0:
+    try:
+        num_terms = int(input("How many terms? "))  
+        if num_terms <= 0:
+            print("Number of terms must be > 0")
+    except ValueError:
+        print("Invalid input. Please enter an integer.")
+
+fib_sequence = [str(fibonacci(i)) for i in range(num_terms)]
+print("Fibonacci sequence:")  
+print(", ".join(fib_sequence))
