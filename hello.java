cdf41d3df8a467b2cb401115bafea8ffb7f4f692
jay-fibi <jay.gohil@fibilabs.tech>
2024-03-13 00:54:31 +0530

    mac comit12

--- hello.java
+++ hello.java
@@ -1,12 +1,24 @@
-public class hello{
-    public static void main(String[] args){
+public class hello {
+    public static void main(String[] args) {
         System.out.println("Hello World");
 
-        //print hello world
-        System.out.println("Hello World");
+        // print Hello World!
+        System.out.println("Hello World!");
+    }
 
-        for(int i=0;i<10;i++){
-            System.out.println("Hello World");
+    public void bubbleSort(int[] array) {
+        for (int i = 0; i < array.length; i++) {
+            for (int j = 0; j < array.length - i - 1; j++) {
+                if (array[j] > array[j + 1]) {
+                    int temp = array[j];
+                    array[j] = array[j + 1];
+                    array[j + 1] = temp;
+                }
+            }
         }
-}
+
+        public static String greet(String name) {
+            return "Hello, " + name + "!";
+        }
+    }
 }
