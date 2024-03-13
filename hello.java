400e3e70bb243ab460a7161849acadac772467bb
jay-fibi <jay.gohil@fibilabs.tech>
2024-03-13 20:46:06 +0530

    multi-file push1

--- hello.java
+++ hello.java
@@ -1,24 +1,5 @@
-public class hello {
+public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello World");
-
-        // print Hello World!
-        System.out.println("Hello World!");
-    }
-
-    public void bubbleSort(int[] array) {
-        for (int i = 0; i < array.length; i++) {
-            for (int j = 0; j < array.length - i - 1; j++) {
-                if (array[j] > array[j + 1]) {
-                    int temp = array[j];
-                    array[j] = array[j + 1];
-                    array[j + 1] = temp;
-                }
-            }
-        }
-
-        public static String greet(String name) {
-            return "Hello, " + name + "!";
-        }
     }
 }
