2e72d5f1b8a1a66d945ceec51a3e3fe414afc967
jay-fibi <jay.gohil@fibilabs.tech>
2024-03-14 00:32:39 +0530

    multi-file push1

---
+++ hello.java
@@ -0,0 +1,17 @@
+public class hello {
+	public static void main(String[] args) {
+		System.out.println("hello world");
+
+		int a =10;
+		int b = 20;
+		int c = a+b;
+		System.out.println(c);
+
+		//print odd or even
+		if (c%2==0) {
+			System.out.println("even");
+		}
+		else {
+			System.out.println("odd");
+		}
+	}
