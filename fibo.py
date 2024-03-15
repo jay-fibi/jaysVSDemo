2723fae470a886ce3f5f95cc2f0879e0ae953376
Jay Gohil <jay.gohil@finilabs.tech>
2024-03-15 23:39:49 +0530

    win push 7mar

--- fibo.py
+++ fibo.py
@@ -1,24 +1,24 @@
-from typing import Dict
+    from typing import Dict
 
-fib_cache: Dict[int, int] = {0: 0, 1: 1} 
+    fib_cache: Dict[int, int] = {0: 0, 1: 1}
 
-def fibonacci(n: int) -> int:
-    if n in fib_cache:
-        return fib_cache[n]
+    def fibonacci(n: int) -> int:
+        if n in fib_cache:
+            return fib_cache[n]
 
-    nth_fib = fibonacci(n-1) + fibonacci(n-2)
-    fib_cache[n] = nth_fib
-    return nth_fib
+        nth_fib = fibonacci(n-1) + fibonacci(n-2)
+        fib_cache[n] = nth_fib
+        return nth_fib
 
-num_terms = 0
-while num_terms <= 0:
-    try:
-        num_terms = int(input("How many terms? "))  
-        if num_terms <= 0:
-            print("Number of terms must be > 0")
-    except ValueError:
-        print("Invalid input. Please enter an integer.")
+    num_terms = 0
+    while num_terms <= 0:
+        try:
+            num_terms = int(input("How many terms? "))
+            if num_terms <= 0:
+                print("Number of terms must be > 0")
+        except ValueError:
+            print("Invalid input. Please enter an integer.")
 
-fib_sequence = [str(fibonacci(i)) for i in range(num_terms)]
-print("Fibonacci sequence:")  
-print(", ".join(fib_sequence))
+    fib_sequence = [str(fibonacci(i)) for i in range(num_terms)]
+    print("Fibonacci sequence:")
+    print(", ".join(fib_sequence))
