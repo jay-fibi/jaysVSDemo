66bf62a0d3870c9d97948c066cfa84d38902e18f
jay-fibi <jay.gohil@fibilabs.tech>
2024-03-22 20:09:44 +0530

    first

--- hello.java
+++ hello.java
@@ -1,17 +1,5 @@
 public class hello {
 	public static void main(String[] args) {
-		System.out.println("hello world");
-
-		int a =10;
-		int b = 20;
-		int c = a+b;
-		System.out.println(c);
-
-		//print odd or even
-		if (c%2==0) {
-			System.out.println("even");
-		}
-		else {
-			System.out.println("odd");
-		}
+		System.out.println("Hello world")
 	}
+}
