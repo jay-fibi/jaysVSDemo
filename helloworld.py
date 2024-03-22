77f2db98d95064a1801db73078f3078317a5a4c2
jay-fibi <jay.gohil@fibilabs.tech>
2024-03-22 22:01:08 +0530

    hello

--- helloworld.py
+++ helloworld.py
@@ -1,9 +1,1 @@
-import datetime
 
-print(datetime.datetime.now())
-
-print(datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S"))
-
-print(datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S.%f"))
-
-print(datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S.%f")[:-3])
