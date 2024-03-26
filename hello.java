9975c6a873bdda67ec54421cc0d05c51f55d0d3a
jay-fibi <jay.gohil@fibilabs.tech>
2024-03-26 20:30:58 +0530

    wwe

--- hello.java
+++ hello.java
@@ -1,43 +1,36 @@
 public class hello{
-	public static void main(String argsp[]){
-		System.out.println("Hello WOrld");
+	public static void main(String[] args){
+		System.out.println("hello world");
 	}
-	/**
-	 * bubbleSort sorts the given integer array using the bubble sort algorithm.
-	 * It iterates through the array multiple times, swapping adjacent elements if
-	 * they are out of order, until the array is sorted.
-	 */
-	public void bubbleSort(int[] array) {
-		int n = array.length;
-		for (int i = 0; i < n; i++) {
-			for (int j = 1; j < (n - i); j++) {
-				if (array[j - 1] > array[j]) {
-					// swap temp and array[j]
-					int temp = array[j - 1];
-					array[j - 1] = array[j];
-					array[j] = temp;
-				}
-			}
+
+	public void mergeSort(int[] arr, int left, int mid, int right){
+		int i = left;
+		int j = mid + 1;
+		int[] temp = new int[right - left + 1];
+		int k = 0;
+		while(i <= mid && j <= right){
+			if(arr[i] <= arr[j]){
+				temp[k++] = arr[i++];
+			}else
+			temp[k++] = arr[j++];
+			System.out.println(Arrays.toString(temp));
+			System.out.println(Arrays.toString(arr));
+			System.out.println("i: " + i + " j: " + j);
+			System.out.println("k: " + k);
+			System.out.println("left: " + left + " mid: " + mid + " right: " + right);
+			System.out.println("=====================");
+			System.out.println();
+			System.out.println();
+			System.out.println();
+			System.out.println();
+			System.out.println();
+		}
+
+	while(i <= mid){
+			temp[k++] = arr[i++];
+		}
+		while(j <= right){
+			temp[k++] = arr[j++];
 		}
 	}
-	public void selectionSort(int[] array) {
-		int n = array.length;
-		for (int i = 0; i < n; i++) {
-			int min_idx = i;
-			for (int j = i + 1; j < n; j++) {
-				if (array[min_idx] > array[j])
-					min_idx = j;
-			}
-			// swap array[i] and array[min_idx]
-			int temp = array[i];
-			array[i] = array[min_idx];
-			array[min_idx] = temp;
-			System.out.println("hello good morning");
-			System.out.println(array[i]);
-			System.out.println(array[min_idx]);
-			System.out.println("hello good night");
-			System.out.println("hello good night");
-			System.out.println("hello good night");
-	}
-}
 }
