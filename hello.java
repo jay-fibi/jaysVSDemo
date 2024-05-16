21378a2bec3d1bb1baed57662d48b7312ac48ba3
jay-fibi <jay.gohil@fibilabs.tech>
2024-05-16 23:06:42 +0530

    this is

--- hello.java
+++ hello.java
@@ -1,17 +1,30 @@
-public class hello {
-	public static void main(String[] args) {
-		System.out.println("hello world");
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+ 	}
 
-		int a =10;
-		int b = 20;
-		int c = a+b;
-		System.out.println(c);
-
-		//print odd or even
-		if (c%2==0) {
-			System.out.println("even");
-		}
-		else {
-			System.out.println("odd");
+	public void bubbleSorter(int[] arr) {
+		for (int i = 0; i < arr.length - 1; i++) {
+			for (int j = 0; j < arr.length - 1 - i; j++) {
+				if (arr[j] > arr[j + 1]) {
+					// Swap arr[j] and arr[j+1]
+					int temp = arr[j];
+					arr[j] = arr[j + 1];
+					arr[j + 1] = temp;
+				}
+			}
 		}
 	}
+	
+
+	public void insertionSort(int[] arr) {
+		for (int i = 1; i < arr.length; i++) {
+			int key = arr[i];
+			int j = i -- 1;
+			while (j >= 0 && arr[j] > key) {
+				arr[j + 1] = arr[j];
+				j--;
+			}
+		}
+	}
+}
