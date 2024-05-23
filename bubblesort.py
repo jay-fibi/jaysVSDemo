5429b3be80f5fc4d9be3d86366ccadd772dceedf
Jay Gohil <jay.gohil@finilabs.tech>
2024-05-23 20:57:43 +0530

    push with 23May-2

--- bubblesort.py
+++ bubblesort.py
@@ -8,18 +8,10 @@ def bubble_sort(arr):
 
 arr = [64, 34, 25, 12, 22, 11, 90]
 
+"""Print each element in array arr to console after sorting.
+
+This is used to visualize the array after it has been sorted by bubble_sort().
+"""
 bubble_sort(arr)
-
-"""
-Prints the sorted and unsorted versions of the given array `arr`.
-
-Args:
-    arr (list): The array to be printed in sorted and unsorted order.
-"""
-print("Sorted array is:")
 for i in range(len(arr)):
-    print("%d" % arr[i]),
-
-print("\n UnSorted array is:")
-for i in range(len(arr)):
-    print("%d" % arr[i]),
+    print(arr[i])
