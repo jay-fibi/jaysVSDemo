8d5857c3de264928f4057b1dca7fe39ca806a1e3
jay-fibi <jay.gohil@fibilabs.tech>
2024-10-05 01:43:43 +0530

    em-2

---
+++ Project-test/first.py
@@ -0,0 +1,9 @@
+# print ("Hello, World!")
+
+# age = 25
+# print(age)
+
+a = input("Enter your name: ")
+b = input("Enter your Age: ")
+
+print (a+ b)
--- hello.java
+++ hello.java
@@ -1,20 +1,14 @@
-public class hello{
-    public static void main(String[] args){
-        System.out.println("Hello World");
+public class hello {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
     }
 
-    // print Jack is Back
-    System.out.println("Jack is Back");
-
-    // print Jay is a nice person..!
-    System.out.println("Jay is a nice person..!");
-
-    public void selectionSort(int[] arr){
+    public void selectionSort(int[] arr) {
         int n = arr.length;
-        for(int i = 0; i < n - 1; i++){
+        for (int i = 0; i < n - 1; i++) {
             int minIndex = i;
-            for(int j = i + 1; j < n; j++){
-                if(arr[j] < arr[minIndex]){
+            for (int j = i + 1; j < n; j++) {
+                if (arr[j] < arr[minIndex]) {
                     minIndex = j;
                 }
             }
@@ -23,11 +17,12 @@ public class hello{
             arr[i] = temp;
         }
     }
-    public void bubbleSort(int[] arr){
+
+    public void bubbleSort(int[] arr) {
         int n = arr.length;
-        for(int i = 0; i < n - 1; i++){
-            for(int j = 0; j < n - i - 1; j++){
-                if(arr[j] > arr[j + 1]){
+        for (int i = 0; i < n - 1; i++) {
+            for (int j = 0; j < n - i - 1; j++) {
+                if (arr[j] > arr[j + 1]) {
                     int temp = arr[j];
                     arr[j] = arr[j + 1];
                     arr[j + 1] = temp;
@@ -35,32 +30,16 @@ public class hello{
             }
         }
     }
-    public void insertionSort(int[] arr){
+    public void insertionSort(int[] arr) {
         int n = arr.length;
-        for(int i = 1; i < n; i++){
+        for (int i = 1; i < n; i++) {
             int key = arr[i];
             int j = i - 1;
-            while(j >= 0 && arr[j] > key){
+            while (j >= 0 && arr[j] > key) {
                 arr[j + 1] = arr[j];
-                j = j - 1;
+                j--;
             }
             arr[j + 1] = key;
         }
     }
-    public void mergeSort(int[] arr){
-        int n = arr.length;
-        if(n < 2){
-            return;
-        }
-        int mid = n / 2;
-        int[] left = new int[mid];
-        int[] right = new int[n - mid];
-        for(int i = 0; i < mid; i++){
-            left[i] = arr[i];
-        }
-        for(int i = mid; i < n; i++){
-            right[i - mid] = arr[i];
-        }
-        mergeSort(left);
-        mergeSort(right);
 }
